9b1ecc3147e6708e2981eaf0458ac564d812e0ec
RaymondWatkin <raymonduscw@gmail.com>
2023-07-13 22:14:03 -0400

    Final Changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -2,14 +2,14 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TranslationTestsEnglish2French(unittest.TestCase):
-    def Test1(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertNotEqual(english_to_french('Hello'), 'Hello')
+    def test1(self):
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertNotEqual(english_to_french('hello'), 'hello')
 
 class TranslationTestsFrench2English(unittest.TestCase):
-    def Test2(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
+    def test2(self):
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
 
-
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,7 +8,7 @@ def english_to_french(english_text):
     Translate English text to French.
 
     """
-    translator = MyMemoryTranslator(source='en', target='fr')
+    translator = MyMemoryTranslator(source='english', target='french')
     french_text = translator.translate(english_text)
     return french_text
 
@@ -17,7 +17,7 @@ def french_to_english(french_text):
     Translate French text to English.
 
     """
-    translator = MyMemoryTranslator(source='fr', target='en')
+    translator = MyMemoryTranslator(source='french', target='english')
     english_text = translator.translate(french_text)
     return english_text
     
